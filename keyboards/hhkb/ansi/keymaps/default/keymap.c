aa52994891e90440a2906bfc996093041364e97b
Taejung <gr8.taejung@gmail.com>
2026-09-06 18:54:25 +0900

    Add custom space key functionality to keymap
    우측 alt 키 한/영 및 Fn 한자 처리
    Ctrl + Space 한영 처리

--- keyboards/hhkb/ansi/keymaps/default/keymap.c
+++ keyboards/hhkb/ansi/keymaps/default/keymap.c
@@ -6,6 +6,12 @@
 #define BASE 0
 #define HHKB 1
 
+// 1. 커스텀 키코드 선언 (기존 키코드와의 번호 충돌 방지를 위해 SAFE_RANGE 사용)
+enum custom_keycodes {
+    CUSTOM_SPACE = SAFE_RANGE,
+};
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /* BASE Level: Default Layer
@@ -20,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      |-------+---+---+---+---+---+---+---+---+---+---+-------+-----+-------+---|
 
             |------+------+-----------------------+------+------|
-            | LAlt | LGUI | ******* Space ******* | RGUI | RAlt |
+            | LAlt | LGUI | ***  CustomSpace  *** | RGUI | 한/영|
             |------+------+-----------------------+------+------|
     */
 
@@ -29,11 +35,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC,
         KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, MO(HHKB),
-        KC_LALT, KC_LGUI, /*        */ KC_SPC, KC_RGUI, KC_RALT),
+        KC_LALT, KC_LGUI, /*        */ CUSTOM_SPC, KC_RGUI, KC_LANG1),
 
     /* Layer HHKB: HHKB mode (HHKB Fn)
       |------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
-      | Pwr  | F1  | F2  | F3  | F4 | F5 | F6 | F7 | F8  | F9  | F10 | F11 | F12   | Ins   | Del |
+      | Menu | F1  | F2  | F3  | F4 | F5 | F6 | F7 | F8  | F9  | F10 | F11 | F12   | Ins   | Del |
       |------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
       | Caps |     |     |     |    |    |    |    | Psc | Slk | Pus | Up  |       | Backs |     |
       |------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
@@ -43,14 +49,45 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       |------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
 
                  |------+------+----------------------+------+------+
-                 | **** | **** | ******************** | **** | **** |
+                 | **** | **** | ******************** | **** | 한자 |
                  |------+------+----------------------+------+------+
 
      */
 
     [HHKB] = LAYOUT(
-        KC_PWR, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_INS, KC_DEL,
+        KC_APP, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_INS, KC_DEL,
         KC_CAPS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_SCRL, KC_PAUS, KC_UP, KC_TRNS, KC_BSPC,
         KC_TRNS, KC_VOLD, KC_VOLU, KC_MUTE, KC_TRNS, KC_TRNS, KC_PAST, KC_PSLS, KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT, KC_PENT,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PPLS, KC_PMNS, KC_END, KC_PGDN, KC_DOWN, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)};
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LANG2)};
+
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case CUSTOM_SPACE:
+            if (record->event.pressed) {
+                // 왼쪽 Ctrl이 눌린 상태에서 Space를 누른 경우
+                if (get_mods() & MOD_BIT(KC_LCTL)) {
+                    unregister_code(KC_LCTL); // Ctrl 신호 잠시 해제
+                    register_code(KC_LANG1);  // 한영키 신호만 전송
+                } else {
+                    // 단독 Space 누름
+                    register_code(KC_SPC);
+                }
+            } else {
+                // 스페이스바 스위치를 뗐을 때 (Release)
+                if (get_mods() & MOD_BIT(KC_LCTL)) {
+                    // Ctrl+Space 조합으로 누른 뒤 뗐을 때
+                    unregister_code(KC_LANG1); // 한영키만 해제
+                    register_code(KC_LCTL);   // 유지 중인 Ctrl 상태 복원
+                } else {
+                    // 단독 Space 누른 뒤 뗐을 때
+                    unregister_code(KC_SPC);   // 스페이스만 해제
+                }
+            }
+            return false; // CUSTOM_SPACE 처리 완료 (QMK 엔진 진입 방지)
+
+        default:
+            return true;
+    }
+}
